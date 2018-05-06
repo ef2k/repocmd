a546ccf4d7a399fc5d06ce3f47e1202c60424763
Eddie Flores <ef2k@users.noreply.github.com>
2018-05-06 19:00:35 -0400

    Get all of the OWNER's repositories

--- main.go
+++ main.go
@@ -2,18 +2,46 @@ package main
 
 import (
 	"context"
+	"log"
 	"os"
 
 	"github.com/shurcooL/githubql"
 	"golang.org/x/oauth2"
 )
 
+type repository struct {
+	Name       githubql.String
+	IsArchived githubql.Boolean
+}
+
 var q struct {
 	Viewer struct {
-		Repository struct {
-			Limit githubql.Int `graphql:"repository(limit:\"30\")"`
-		}
+		Repositories struct {
+			Nodes    []repository
+			PageInfo struct {
+				EndCursor   githubql.String
+				HasNextPage githubql.Boolean
+			}
+		} `graphql:"repositories(first:100, after:$repositoriesCursor, affiliations:[OWNER])"`
 	}
+}
+
+func getRepositories(client *githubql.Client) []repository {
+	variables := map[string]interface{}{
+		"repositoriesCursor": (*githubql.String)(nil),
+	}
+	var repos []repository
+	for {
+		if err := client.Query(context.Background(), &q, variables); err != nil {
+			log.Fatal(err)
+		}
+		repos = append(repos, q.Viewer.Repositories.Nodes...)
+		if !q.Viewer.Repositories.PageInfo.HasNextPage {
+			break
+		}
+		variables["repositoriesCursor"] = githubql.NewString(q.Viewer.Repositories.PageInfo.EndCursor)
+	}
+	return repos
 }
 
 func main() {
@@ -22,4 +50,7 @@ func main() {
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
 	client := githubql.NewClient(httpClient)
+
+	repos := getRepositories(client)
+	log.Printf("Got all repositories (%d)\n", len(repos))
 }
