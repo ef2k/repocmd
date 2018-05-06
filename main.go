969c43d030d48f9db0db62fcb56137fb477312ab
Eddie Flores <ef2k@users.noreply.github.com>
2018-05-06 18:01:37 -0400

    Vendor gh graphql deps

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"github.com/google/go-github/github"
+	"context"
+	"os"
+
+	"github.com/shurcooL/githubql"
+	"golang.org/x/oauth2"
 )
 
+var q struct {
+	Viewer struct {
+		Repository struct {
+			Limit githubql.Int `graphql:"repository(limit:\"30\")"`
+		}
+	}
+}
+
 func main() {
-	client := github.NewClient(nil)
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	client := githubql.NewClient(httpClient)
 }
