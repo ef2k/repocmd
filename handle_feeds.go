f1fc250f6aae5cdce50d62859926a8da9c3c8668
Eddie Flores <ef2k@users.noreply.github.com>
2018-06-04 19:12:10 -0400

    Parse an atom feed, return it as json

--- handle_feeds.go
+++ handle_feeds.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/mmcdole/gofeed/atom"
 )
 
 func handleGetFeed(w http.ResponseWriter, r *http.Request) {
@@ -15,10 +17,19 @@ func handleGetFeed(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		http.Error(w, err.Error(), resp.StatusCode)
 		return
 	}
-	copyResponse(w, resp)
+
+	fp := atom.Parser{}
+	feed, _ := fp.Parse(resp.Body)
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(feed); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
